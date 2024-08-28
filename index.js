4c45f784968112b06106fd68b54c84e45bf97780
HarshitJainPD <harshitjain@productstack.in>
2024-08-29 00:00:21 +0530

    dummy commit

--- index.js
+++ index.js
@@ -30,5 +30,5 @@ const server = http.createServer((req, res) => {
 app.listen(3000, () => {
   console.log(`Server running on port 3000`);
 }).on('error', (err) => {
-  console.error('Failed to start server:', err);
+  console.error('Failed to start the server:', err);
 });
